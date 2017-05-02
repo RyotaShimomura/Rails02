a4985fbe7d71bbea90fed35340a37fae275c8dee
Ryota Shimomura <s1713111@s.tsukuba.ac.jp>
2017-05-02 16:19:04 +0900

    演習デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def hello
       render html: "hello, world!"
     end
+    
+  def goodbye
+        render html: "goodbye, world!"
+      end
 end
